ec28b14bbac5d53bf1e18ddf3cca76e1fde7f2d1
Malycia Adriana Matthew Ashley <malyciaashley@gmail.com>
2024-06-27 06:56:34 -0700

    Develop and design contact nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-// icons for links, TAILWIND (try import or install instead?, can use for dark mode!), design current page!
+// TAILWIND (try import or install instead?, can use for dark mode!), design current page + create hover states, update .json files :) 
 
 function App() {
   return (
@@ -10,9 +10,15 @@ function App() {
       <p>UI Designer | Proficient in Figma | Teamwork | Accessibility</p>
       <div className="contact-nav">
         <a id="resume-button" href="https://docs.google.com/document/d/1XbUdY7ef-mh0TaYS7mJinEW-JWX-bpb2kg_1pnk-ObU/">Resume</a>
-        <a className="contact-navbtn" href="https://www.linkedin.com/in/malycia-ashley/"></a>
-        <a className="contact-navbtn" href="mailto:malyciaashley@gmail.com"></a>
-        <a className="contact-navbtn" href="https://github.com/FireBreather65"></a>
+        <a className="contact-navbtn" href="https://www.linkedin.com/in/malycia-ashley/">
+          <img src="https://cloud-o9etpd58m-hack-club-bot.vercel.app/2mdi_linkedin.svg" alt="LinkedIn icon" />
+        </a>
+        <a className="contact-navbtn" href="mailto:malyciaashley@gmail.com">
+          <img src="https://cloud-o9etpd58m-hack-club-bot.vercel.app/0material-symbols_mail-rounded.svg" alt="Mail icon" />
+        </a>
+        <a className="contact-navbtn" href="https://github.com/FireBreather65">
+          <img src="https://cloud-o9etpd58m-hack-club-bot.vercel.app/1mdi_github.svg" alt="GitHub icon" />
+        </a>
       </div>
     </div>
   );
