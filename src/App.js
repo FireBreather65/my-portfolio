d956cae990d8da4bb88939d7beec26df9dbb2e4f
Malycia Adriana Matthew Ashley <malyciaashley@gmail.com>
2024-06-27 08:59:29 -0700

    Add todos, nav content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 
-// TAILWIND (try import or install instead?, can use for dark mode!), design current page + create hover states, update .json files :) 
+// Today's todos: add all content (esp. section-nav!), set up Tailwind, update .json files :)
 
 function App() {
   return (
     <div className="body">
-      <img src="https://cloud-dr0rikspp-hack-club-bot.vercel.app/0untitled_design__1_.png" alt="Mountain at sunset in Olympia, Washington" />
-      <h1 className="header-title-name">Malycia Ashley</h1>
+      <div className="header">
+        <img src="https://cloud-pxmactc09-hack-club-bot.vercel.app/0bi_moon.svg" alt="Dark mode icon" />
+        <img src="https://cloud-pxmactc09-hack-club-bot.vercel.app/1bi_sun.svg" alt="Light mode icon" />
+        <img src="https://cloud-dr0rikspp-hack-club-bot.vercel.app/0untitled_design__1_.png" alt="Mountain at sunset in Olympia, Washington" />
+      </div>
+      <img src="https://cloud-i5wn1rmma-hack-club-bot.vercel.app/0pxl_20240220_005648933.jpg" alt="Picture of me in front of trees" />
+      <h1 className="name-pronouns">Malycia Ashley</h1>
       <p>UI Designer | Proficient in Figma | Teamwork | Accessibility</p>
       <div className="contact-nav">
         <a id="resume-button" href="https://docs.google.com/document/d/1XbUdY7ef-mh0TaYS7mJinEW-JWX-bpb2kg_1pnk-ObU/">Resume</a>
@@ -18,6 +23,24 @@ function App() {
         </a>
         <a className="contact-navbtn" href="https://github.com/FireBreather65">
           <img src="https://cloud-o9etpd58m-hack-club-bot.vercel.app/1mdi_github.svg" alt="GitHub icon" />
+        </a>
+      </div>
+      <div className="section-nav">
+        <a className="section-navitm">
+          <img src="" alt="" />
+          <p></p>
+        </a>
+        <a className="section-navitm">
+          <img src="" alt="" />
+          <p></p>
+        </a>
+        <a className="section-navitm">
+          <img src="" alt="" />
+          <p></p>
+        </a>
+        <a className="section-navitm">
+          <img src="" alt="" />
+          <p></p>
         </a>
       </div>
     </div>
