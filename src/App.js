6ad4fcf04ecdffa88f4968a7d6626fdf39ef5867
Malycia Adriana Matthew Ashley <malyciaashley@gmail.com>
2024-06-27 20:49:01 -0700

    Update CSS, implement Tailwind!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-// Today's todos: add all content (esp. section-nav!), set up Tailwind, update .json files :)
+// make sure it's good enough for presentation (bare bones but still good, update selectors for images to be less/more specific!!!)
 
 function App() {
   return (
@@ -15,34 +15,37 @@ function App() {
       <p>UI Designer | Proficient in Figma | Teamwork | Accessibility</p>
       <div className="contact-nav">
         <a id="resume-button" href="https://docs.google.com/document/d/1XbUdY7ef-mh0TaYS7mJinEW-JWX-bpb2kg_1pnk-ObU/">Resume</a>
-        <a className="contact-navbtn" href="https://www.linkedin.com/in/malycia-ashley/">
+        <a className="contact-navbtn" href="https://www.linkedin.com/in/malycia-ashley/" target="_blank">
           <img src="https://cloud-o9etpd58m-hack-club-bot.vercel.app/2mdi_linkedin.svg" alt="LinkedIn icon" />
         </a>
-        <a className="contact-navbtn" href="mailto:malyciaashley@gmail.com">
+        <a className="contact-navbtn" href="mailto:malyciaashley@gmail.com" target="_blank">
           <img src="https://cloud-o9etpd58m-hack-club-bot.vercel.app/0material-symbols_mail-rounded.svg" alt="Mail icon" />
         </a>
-        <a className="contact-navbtn" href="https://github.com/FireBreather65">
+        <a className="contact-navbtn" href="https://github.com/FireBreather65" target="_blank">
           <img src="https://cloud-o9etpd58m-hack-club-bot.vercel.app/1mdi_github.svg" alt="GitHub icon" />
         </a>
       </div>
       <div className="section-nav">
-        <a className="section-navitm">
-          <img src="" alt="" />
-          <p></p>
+        <a className="section-navitm" href="#">
+          <img src="https://cloud-i3mm96q23-hack-club-bot.vercel.app/0priscilla-du-preez-k8xygbw4ahg-unsplash__1_.jpg" alt="Two people holding coffee" />
+          <p>About</p>
         </a>
-        <a className="section-navitm">
-          <img src="" alt="" />
-          <p></p>
+        <a className="section-navitm" href="#">
+          <img src="https://cloud-i3mm96q23-hack-club-bot.vercel.app/1ian-dooley-dj7bwa-gwks-unsplash__1_.jpg" alt="Tech setup with laptop and coffee" />
+          <p>Experience</p>
         </a>
-        <a className="section-navitm">
-          <img src="" alt="" />
-          <p></p>
+        <a className="section-navitm" href="#">
+          <img src="https://cloud-i3mm96q23-hack-club-bot.vercel.app/3goran-ivos-ioykdikzlqw-unsplash__1_.jpg" alt="Laptop on an ottoman" />
+          <p>Projects</p>
         </a>
-        <a className="section-navitm">
-          <img src="" alt="" />
-          <p></p>
+        <a className="section-navitm" href="#">
+          <img src="https://cloud-i3mm96q23-hack-club-bot.vercel.app/2rirri-p_swqcxuc1k-unsplash__1_.jpg" alt="Polaroids in a scrapbook" />
+          <p>Scrapbook</p>
         </a>
       </div>
+      <footer>
+        <p>Made with ❤️ by Malycia Ashley</p>
+      </footer>
     </div>
   );
 }
