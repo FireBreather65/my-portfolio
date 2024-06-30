08872eecfdc6f750d1cd8922fa9de675e040ac16
Malycia Adriana Matthew Ashley <malyciaashley@gmail.com>
2024-06-30 13:58:38 -0700

    Create todo list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+{/* Todos:
+  - fix nav orientation (currently up to down)
+  - automatically resize selfie to be half of pic (if possible)
+  - CREATE MODAL (add bg, shadows, etc.)
+  */}
 
 function App() {
   return (
     <div class="min-w-64 w-1/2 my-20 mx-auto text-center font-sans" style={{fontFamily: 'DM Sans'}}>
-      {/* ↑↑ CREATE MODAL ↑↑ */}
       {/* ↓↓ future implementation ↓↓ */}
       <div className="light-dark-mode">
         {/*<img src="https://cloud-pxmactc09-hack-club-bot.vercel.app/0bi_moon.svg" alt="Dark mode icon" class="w-8 h-8 border-2 border-blue-400 border-solid rounded ml-4 inline-block align-middle" />
@@ -13,7 +16,7 @@ function App() {
         <img src="https://cloud-dr0rikspp-hack-club-bot.vercel.app/0untitled_design__1_.png" alt="Mountain at sunset in Olympia, Washington" class="w-full object-cover" />
       </header>
       <div className="header-content" class="my-8 text-center">
-        <img src="https://cloud-i5wn1rmma-hack-club-bot.vercel.app/0pxl_20240220_005648933.jpg" alt="Picture of me in front of trees" class="w-36 h-36 -mt-24 rounded-full border-4 border-white border-solid object-cover inline" /> {/* <- automatically resize to be half of pic, if possible */}
+        <img src="https://cloud-i5wn1rmma-hack-club-bot.vercel.app/0pxl_20240220_005648933.jpg" alt="Picture of me in front of trees" class="w-36 h-36 -mt-24 rounded-full border-4 border-white border-solid object-cover inline" />
         <h1 class="text-3xl font-bold">Malycia Ashley <span class="text-xl opacity-40 inline align-middle">(they/them)</span></h1>
         <p>UI Designer | Proficient in Figma | Teamwork | Accessibility</p>
       </div>
@@ -29,7 +32,6 @@ function App() {
           <img src="https://cloud-o9etpd58m-hack-club-bot.vercel.app/1mdi_github.svg" alt="GitHub icon" class="w-8 h-8 border-2 border-blue-400 border-solid rounded ml-4 inline-block align-middle" />
         </a>
       </div>
-      {/* fix orientation, currently up to down */}
     <div className="section-nav" class="flex-row">
         <a href="#">
           <img src="https://cloud-i3mm96q23-hack-club-bot.vercel.app/0priscilla-du-preez-k8xygbw4ahg-unsplash__1_.jpg" alt="Two people holding coffee" class="w-16 h-16 rounded-full inline-block text-center m-auto object-cover" />
