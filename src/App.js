934b493d694a010f348c1ddd3f4c9f0502a8bf11
Malycia Adriana Matthew Ashley <malyciaashley@gmail.com>
2024-07-01 06:40:53 -0700

    Resize header image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 {/* Todos:
-  - CREATE MODAL (add bg, shadows, etc.)
   - realign text for nav
-  - automatically resize selfie to be half of pic (if possible)
+  - automatically position selfie to be half of pic (if possible)
   - add section pages + content
   - add functionality to buttons
   - make images less fuzzy XD
@@ -11,7 +10,7 @@
 function App() {
   return (
     <div class="min-w-64 w-1/2 my-20 mx-auto text-center font-sans bg-orange-100 rounded shadow-lg pb-4" style={{fontFamily: 'DM Sans'}}>
-      <img src="https://cloud-dr0rikspp-hack-club-bot.vercel.app/0untitled_design__1_.png" alt="Mountain at sunset in Olympia, Washington" class="w-full object-cover rounded-t shadow-lg" />
+      <img src="https://cloud-dr0rikspp-hack-club-bot.vercel.app/0untitled_design__1_.png" alt="Mountain at sunset in Olympia, Washington" class="w-full object-cover rounded-t shadow-lg h-80" />
       <div className="header-content" class="my-8 text-center">
         <img src="https://cloud-i5wn1rmma-hack-club-bot.vercel.app/0pxl_20240220_005648933.jpg" alt="Picture of me in front of trees" class="w-36 h-36 -mt-24 rounded-full border-4 border-orange-100 border-solid object-cover inline" />
         <h1 class="text-3xl font-bold">Malycia Ashley <span class="text-xl opacity-40 inline align-middle">(they/them)</span></h1>
@@ -32,20 +31,20 @@ function App() {
         </a>
       </div>
       <div className="section-nav" class="m-8">
-          <a href="https://firebreather65.github.io/hc-workshop-wonderland/" target="_blank" class="inline-block align-middle ml-4">
+          <a href="https://firebreather65.github.io/hc-workshop-wonderland/" target="_blank" class="inline-block align-center mx-2 my-auto">
             <img src="https://cloud-i3mm96q23-hack-club-bot.vercel.app/0priscilla-du-preez-k8xygbw4ahg-unsplash__1_.jpg" alt="Two people holding coffee" class="w-16 h-16 rounded-full object-cover" />
             <p>About</p>
           </a>
-          <a href="https://firebreather65.github.io/hc-workshop-wonderland/" target="_blank" class="inline-block align-middle ml-4">
+          <a href="https://firebreather65.github.io/hc-workshop-wonderland/" target="_blank" class="inline-block align-center mx-2 my-auto">
             <img src="https://cloud-i3mm96q23-hack-club-bot.vercel.app/1ian-dooley-dj7bwa-gwks-unsplash__1_.jpg" alt="Tech setup with laptop and coffee" class="w-16 h-16 rounded-full object-cover" />
             <p>Experience</p>
           </a>
-        <a href="https://firebreather65.github.io/hc-workshop-wonderland/" target="_blank" class="inline-block align-middle ml-4">
+        <a href="https://firebreather65.github.io/hc-workshop-wonderland/" target="_blank" class="inline-block align-center mx-2 my-auto">
           <img src="https://cloud-i3mm96q23-hack-club-bot.vercel.app/3goran-ivos-ioykdikzlqw-unsplash__1_.jpg" alt="Laptop on an ottoman" class="w-16 h-16 rounded-full object-cover" />
           <p>Projects</p>
         </a>
-        <a href="https://firebreather65.github.io/hc-workshop-wonderland/" target="_blank" class="inline-block align-middle ml-4">
-          <img src="https://cloud-i3mm96q23-hack-club-bot.vercel.app/2rirri-p_swqcxuc1k-unsplash__1_.jpg" alt="Polaroids in a scrapbook" class="w-16 h-16 rounded-full object-cover " />
+        <a href="https://firebreather65.github.io/hc-workshop-wonderland/" target="_blank" class="inline-block align-center mx-2 my-auto">
+          <img src="https://cloud-i3mm96q23-hack-club-bot.vercel.app/2rirri-p_swqcxuc1k-unsplash__1_.jpg" alt="Polaroids in a scrapbook" class="w-16 h-16 rounded-full object-cover" />
           <p>Scrapbook</p>
         </a>
     </div>
