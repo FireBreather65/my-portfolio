6eee1ff030174f356966491af51585585b40b247
Malycia Adriana Matthew Ashley <malyciaashley@gmail.com>
2024-07-02 13:27:15 -0700

    Finish home page!

--- src/App.js
+++ src/App.js
@@ -20,26 +20,24 @@ function App() {
       <img src="https://cloud-dr0rikspp-hack-club-bot.vercel.app/0untitled_design__1_.png" alt="Mountain at sunset in Olympia, Washington" class="w-full object-cover rounded-t-2xl shadow-lg h-80" />
 
       {/* HEADER SECTION */}
-      <div className="header-content" class="my-8 text-center">
+      <div className="header-content" class="my-6 text-center">
         <img src="https://cloud-i5wn1rmma-hack-club-bot.vercel.app/0pxl_20240220_005648933.jpg" alt="Picture of me in front of trees" class="w-36 h-36 -mt-24 rounded-full border-4 border-orange-100 border-solid object-cover inline" />
         <h1 class="text-3xl font-bold">Malycia Ashley <span class="text-xl opacity-40 inline align-middle">(they/them)</span></h1>
-        <p>UI Designer | Proficient in Figma | Teamwork | Accessibility</p>
-      </div>
-
-      {/* CONTACT SECTION */}
-      <div className="contact-nav" class="m-8">
-        <a href="https://docs.google.com/document/d/1XbUdY7ef-mh0TaYS7mJinEW-JWX-bpb2kg_1pnk-ObU/edit?usp=sharing" target="_blank">
-          <img src="https://cloud-62u98qd5f-hack-club-bot.vercel.app/0mdi_resume.svg" alt="Resume icon" class="w-8 h-8 ml-4 inline-block align-middle" />
-        </a>
-        <a href="https://www.linkedin.com/in/malycia-ashley/" target="_blank">
-          <img src="https://cloud-o9etpd58m-hack-club-bot.vercel.app/2mdi_linkedin.svg" alt="LinkedIn icon" class="w-8 h-8 ml-4 inline-block align-middle" />
-        </a>
-        <a href="mailto:malyciaashley@gmail.com" target="_blank">
-          <img src="https://cloud-i6vrdd91r-hack-club-bot.vercel.app/0mdi_email.svg" alt="Mail icon" class="w-8 h-8 ml-4 inline-block align-middle" />
-        </a>
-        <a href="https://github.com/FireBreather65" target="_blank">
-          <img src="https://cloud-o9etpd58m-hack-club-bot.vercel.app/1mdi_github.svg" alt="GitHub icon" class="w-8 h-8 ml-4 inline-block align-middle" />
-        </a>
+        <p class="m-auto w-2/5">UI Designer | Proficient in Figma | Teamwork | Accessibility</p>
+        <div className="contact-nav" class="my-2">
+          <a href="https://docs.google.com/document/d/1XbUdY7ef-mh0TaYS7mJinEW-JWX-bpb2kg_1pnk-ObU/edit?usp=sharing" target="_blank">
+            <img src="https://cloud-62u98qd5f-hack-club-bot.vercel.app/0mdi_resume.svg" alt="Resume icon" class="w-8 h-8 ml-4 inline-block align-middle" />
+          </a>
+          <a href="https://www.linkedin.com/in/malycia-ashley/" target="_blank">
+            <img src="https://cloud-o9etpd58m-hack-club-bot.vercel.app/2mdi_linkedin.svg" alt="LinkedIn icon" class="w-8 h-8 ml-4 inline-block align-middle" />
+          </a>
+          <a href="mailto:malyciaashley@gmail.com" target="_blank">
+            <img src="https://cloud-i6vrdd91r-hack-club-bot.vercel.app/0mdi_email.svg" alt="Mail icon" class="w-8 h-8 ml-4 inline-block align-middle" />
+          </a>
+          <a href="https://github.com/FireBreather65" target="_blank">
+            <img src="https://cloud-o9etpd58m-hack-club-bot.vercel.app/1mdi_github.svg" alt="GitHub icon" class="w-8 h-8 ml-4 inline-block align-middle" />
+          </a>
+        </div>
       </div>
 
       {/* NAV SECTION */}
