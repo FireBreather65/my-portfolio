5e3bf00c1fd91ec004d067973cc6099be7c3f03f
Malycia <76709163+FireBreather65@users.noreply.github.com>
2024-09-09 16:38:13 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -10,29 +10,13 @@ function About() {
       >
         <h1 class="text-3xl font-bold p-4">About</h1>
         <p class="w-1/2 mx-auto pb-4">
-          I'm Malycia, a designer who's been creating responsive wireframes and
-          prototypes, initiating UI kits, and reviewing user flows since
-          September 2023.
+        Hey! I'm Malycia, a designer who's been creating responsive wireframes and prototypes, initiating UI kits, and reviewing user flows since September 2023 :)
+        <br />
+        <br />
+        I've worked with many teams to redesign their existing website, or even create it from scratch. In all of these teams, I've had to create a space for designers that wasn't previously fleshed out by showcasing my skills and their benefits to the team's image.
           <br />
           <br />
-          I've worked with many teams to redesign their existing website, or
-          even create it from scratch. In all of these teams I've had to create
-          a space for designers that wasn't previously fleshed out by showcasing
-          my skills and their benefits to the team's image.
-          <br />
-          <br />I also have some side projects! With the support of Hack Club, I
-          help organize hackathons across the US for girls and gender-diverse
-          students to be introduced to tech. You can <a
-            href="https://daysofservice.hackclub.com/"
-            target="_blank"
-            class="underline text-blue-600 justify-center"
-          >
-            learn more about us here
-          </a>.
-          <br />
-          <br />
-          If you are interested in hiring me for any design opportunities, I'd
-          love to hear from you. You can contact me via email or LinkedIn DM.
+I'm also very passionate about bringing more diverse perspectives into tech. I've previously done work with Hack Club (a non-profit org for teens in tech) to bring more girls and non-binary students into the space! I've hosted hackathons and networking dinners across the US and have worked to make a real impact in participants' lives.
         </p>
       </div>
     </main>
