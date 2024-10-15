7fd97467ae49155f3defcfa2ec4b87db1b5b45f0
Malycia <76709163+FireBreather65@users.noreply.github.com>
2024-10-15 14:20:38 -0700

    Revert back to original image

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ function App() {
         style={{ fontFamily: "DM Sans" }}
       >
         <img
-          src="flower_in_the_pond.jpg"
-          alt="Flower in a pond"
+          src="https://cloud-dr0rikspp-hack-club-bot.vercel.app/0untitled_design__1_.png"
+          alt="Mountain at sunset in Olympia, Washington"
           class="w-full object-cover rounded-t-2xl shadow-lg h-80"
         />
 
