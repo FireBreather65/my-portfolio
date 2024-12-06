fa3af816d34d438dc4ab55f165b8a80a497ebd24
Malycia <76709163+FireBreather65@users.noreply.github.com>
2024-12-05 20:38:58 -0800

    Update word choice

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ function About() {
         Ever since I was a kid, I knew I wanted a job in a creative field. Through the years I tried dabbling in writing, beading, origami, even piano - but when I graduated high school at 16 and discovered the field of UX design, I knew I had found my calling. I fell in love with designing interfaces and the ability to use my natural creativity to positively impact users and the world.
         <br />
         <br />
-        My past jobs have taught me the importance of collaboration, communication, and attention to detail - all crucial skills in my journey into UX. I’m also committed to equity-focused design, as I've learned that hearing from diverse perspectives can positively impact the experience for all users.
+        My past jobs have taught me the importance of collaboration, communication, and attention to detail - all crucial skills in my journey into UX. I’m also committed to equity-focused design, as I've found that hearing from diverse perspectives can positively impact the experience for all users.
         </p>
       </div>
     </main>
