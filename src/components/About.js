2d433d1c0c65b0bd40fbff9361ec4fec526da026
Malycia <76709163+FireBreather65@users.noreply.github.com>
2024-12-13 23:12:33 -0800

    Update wording

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ function About() {
       >
         <h1 class="text-3xl font-bold p-4">About</h1>
         <p class="w-1/2 mx-auto pb-4">
-        Ever since I was a kid, I knew I wanted a job in a creative field. Through the years I tried dabbling in writing, beading, origami, even piano - but when I graduated high school at 16 and discovered the field of UX design, I knew I had found my calling. I fell in love with designing interfaces and the ability to use my natural creativity to positively impact users and the world.
+        Ever since I was a kid, I knew I wanted a job in a creative field. Through the years I tried dabbling in writing, beading, origami, even piano - but when I obtained my GED at 16 and discovered the field of UX design, I knew I had found my calling. I fell in love with designing interfaces and the ability to use my natural creativity to positively impact users and the world.
         <br />
         <br />
         My past jobs have taught me the importance of collaboration, communication, and attention to detail - all crucial skills in my journey into UX. I’m also committed to equity-focused design, as I've found that hearing from diverse perspectives can positively impact the experience for all users.
